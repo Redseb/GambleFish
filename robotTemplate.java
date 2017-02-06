56e578c94c67c21bc44a42ce4ab019b2f7e66004
Miko Zyzanski <dwarfslikepie@gmail.com>
2017-02-06 14:15:55 +0100

    Update robotTemplate.java

--- robotTemplate.java
+++ robotTemplate.java
@@ -8,12 +8,12 @@ public class robotTemplate {
   public static void main(String[] argv) throws Exception {
     Robot robot = new Robot();//Define new object called Robot
     int i = 0;
-    robot.delay(5000);//5 Seconds wait to move mouse
-    while(i<2000)
+    robot.delay(5000);//5 Seconds wait to move mouse (It's in miliseconds)
+    while(i<2000) //2000 determines how 'long' it takes
       {
-        robot.mousePress(InputEvent.BUTTON1_MASK);
+        robot.mousePress(InputEvent.BUTTON1_MASK);//Presses button
         robot.delay(100);
-        robot.mouseRelease(InputEvent.BUTTON1_MASK);
+        robot.mouseRelease(InputEvent.BUTTON1_MASK);//Releases button
         i++;
       }
   }
